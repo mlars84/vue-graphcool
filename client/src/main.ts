5780c180c6783086d4ec1b2a8e9b129c5319a2b2
Matt Larson <matt.a.larson@gmail.com>
2018-06-14 07:56:31 -0500

    setup apollo

--- client/src/main.ts
+++ client/src/main.ts
@@ -6,10 +6,32 @@ import './registerServiceWorker';
 import 'tachyons';
 import { ApolloClient } from 'apollo-client';
 import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import VueApollo from 'vue-apollo';
 
 Vue.config.productionTip = false;
 
+const httpLink = new HttpLink({
+  uri: 'https://api.graph.cool/simple/v1/cjiej2mm4bbde01457mpsd1ie'
+})
+
+const apolloClient = new ApolloClient({
+  link: httpLink,
+  cache: new InMemoryCache(),
+  connectToDevTools: true
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+  defaultOptions: {
+    $loadingKey: 'loading'
+  }
+})
+
 new Vue({
+  provide: apolloProvider.provide(),
   router,
   store,
   render: (h) => h(App),
