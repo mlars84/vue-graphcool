89cbfc2589490b8746c76a1ec34c0ecd69525d19
Matt Larson <matt.a.larson@gmail.com>
2018-06-14 08:04:01 -0500

    setting up link component

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <LinkItem link="some link" />
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import LinkItem from '@/components/LinkItem.vue';
 
 @Component({
   components: {
-    HelloWorld,
+    LinkItem,
   },
 })
 export default class Home extends Vue {}
